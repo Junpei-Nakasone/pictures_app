b9b456fdfc8b5e781d0e81f3c50ce151f671c211
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2020-12-27 16:30:30 +0900

    フロントエンドのログイン実装

--- client/src/components/LatestPosts.vue
+++ client/src/components/LatestPosts.vue
@@ -21,12 +21,8 @@ export default {
     }
   },
   async mounted() {
-    // alert('mounted')
-    // axios.get('http://localhost:9999/fetchLatestImages')
-    // axios.get('http://18.180.158.120:9999/fetchLatestImages')
     api.get('/fetchLatestImages')
     .then((res) => {
-      // alert('fetchlatestImages')
       console.log(res)
       this.latestPosts = res.data
     })
