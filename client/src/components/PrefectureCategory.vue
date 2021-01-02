ec0f985623729a66a92fd12983a93ed8233f95ca
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2021-01-03 07:48:56 +0900

    都道府県別画像表示UI実装

--- client/src/components/PrefectureCategory.vue
+++ client/src/components/PrefectureCategory.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <v-container fluid>
     <v-row align="center">
       <v-col
@@ -7,18 +8,55 @@
         sm="6"
       >
         <v-select
-          :items="items"
+          :items="prefectureCategories"
+          item-text="prefecture_name"
+          item-value="prefecture_category_cd"
           label="都道府県"
+          v-model="items"
+          @change="fetchImageByPrefectureCd(items)"
         ></v-select>
       </v-col>
     </v-row>
   </v-container>
+  <PostList
+    :postType="prefecturePosts"
+    />
+  </div>
 </template>
 <script>
+import PostList from '@/components/PostList'
+import axios from 'axios'
+import api from '@/services/api'
+
+
 export default {
-    data: () => ({
-      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    }),
+  components: {
+    PostList,
+  },
+    data() {
+      return {
+        prefectureCategories: [],
+        prefecturePosts: []
+      }
+    },
+    async mounted() {
+      api.get('/fetchPrefectureCategories')
+      .then((res) => {
+        this.prefectureCategories = res.data
+        console.log(this.prefectureCategories)
+      })
+    },
+    methods: {
+      fetchImageByPrefectureCd(value) {
+        api.post('/fetchImageByPrefectureCd',{
+          'prefecture_category_cd': value
+        })
+        .then((res) => {
+          console.log(res)
+          this.prefecturePosts = res.data
+        })
+      }
+    }
   }
 </script>
 
