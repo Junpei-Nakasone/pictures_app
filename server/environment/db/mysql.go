a65924552465fb60a8d149e29a59e5867b746d7f
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2021-01-20 23:24:21 +0900

    接続情報を修正

--- server/environment/db/mysql.go
+++ server/environment/db/mysql.go
@@ -14,19 +14,19 @@ func CreateDBConnection() *gorm.DB {
 	// DBMS := "mysql"
 	DBMS := os.Getenv("DBMS")
 	// USER := "root"
-	USER := os.Getenv("USER")
+	// USER := os.Getenv("USER")
 	// PASSWORD := "root"
-	PASSWORD := os.Getenv("PASSWORD")
-	// USER := "admin"
-	// PASSWORD := "password"
+	// PASSWORD := os.Getenv("PASSWORD")
+	USER := "admin"
+	PASSWORD := "password"
 	// ローカルのGoからDockerのMySQLに繋ぐ時はコンテナ名ではなくlocalhostで指定する
 	// PROTOCOL := "tcp(localhost:3306)"
-	PROTOCOL := os.Getenv("PROTOCOL")
+	// PROTOCOL := os.Getenv("PROTOCOL")
 	// PROTOCOL := "tcp(mysql:3306)"
 	// RDS
-	// PROTOCOL := "tcp(rds-pictures-0106.ci3srwjbei5c.ap-northeast-1.rds.amazonaws.com:3306)"
-	// DBNAME := "test_database"
-	DBNAME := os.Getenv("DBNAME")
+	PROTOCOL := "tcp(rds-pictures-0106.ci3srwjbei5c.ap-northeast-1.rds.amazonaws.com:3306)"
+	DBNAME := "test_database"
+	// DBNAME := os.Getenv("DBNAME")
 	CONNECT := USER + ":" + PASSWORD + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
 	db, err := gorm.Open(DBMS, CONNECT)
 	// db, err := gorm.Open("mysql", "admin:password@tcp(database-1.ci3srwjbei5c.ap-northeast-1.rds.amazonaws.com:3306)/test_database?parseTime=true")
