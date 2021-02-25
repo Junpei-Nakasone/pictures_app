3b8d1dd8e2f71b064d38b37ee34b314ec0fd2579
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2021-02-26 06:42:12 +0900

    データを追加、cfnテンプレートを修正

--- client/src/mixin/mixin.js
+++ client/src/mixin/mixin.js
@@ -2,30 +2,35 @@ export default {
   methods: {
     showLoginSeccessMessage() {
       this.$buefy.toast.open({
+        duration: 5000,
         message: 'ログインしました',
         type: 'is-success'
       })
     },
     showLoginFailMessage() {
       this.$buefy.toast.open({
+        duration: 5000,
         message: 'ユーザー名かパスワード名が間違っています',
         type: 'is-danger'
       })
     },
     showSignupSuccessMessage() {
       this.$buefy.toast.open({
+        duration: 5000,
         message: '新規登録完了しました',
         type: 'is-success'
       })
     },
     showSignupFailMessage() {
       this.$buefy.toast.open({
+        duration: 5000,
         message: '新規登録に失敗しました',
         type: 'is-danger'
       })
     },
     showLogoutMessage() {
       this.$buefy.toast.open({
+        duration: 5000,
         message: 'ログアウトしました',
         type: 'is-info'
       })
